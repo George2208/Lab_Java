56982b6cea6d749d370b568f28d9e3ecba225b8b
George2208 <56531619+George2208@users.noreply.github.com>
2021-04-26 22:09:24 +0300

    Delete Proiect 2 directory

--- Proiect 2/TableRow.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-import java.util.List;
-import java.util.Map;
-
-public interface TableRow {
-    List<String> getData();
-    <T extends TableRow> void addData(Map<Integer, T> map, String[] columns) throws IndexOutOfBoundsException;
-}
